c458b6f84161af0ee8553ef344e6f25d8ae02be2
George Zalepa <george.zalepa@gmail.com>
2023-09-26 14:13:16 -0400

    Setup tailwind and clobber existing pages

--- src/index.js
+++ src/index.js
@@ -8,31 +8,7 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-
-  function calculateDisplaySizes(artworks) {
-
-    const minWidth = 40;
-
-    const widest = artworks.reduce((max, o) => {
-      return parseInt(o.width) > parseInt(max.width) ? o : max
-    });
-
-    const augmentedArtworks = artworks.map(a => {
-      a.displayPercentage = parseInt(a.width) * 100 / parseInt(widest.width) ;
-      if (a.displayPercentage < 25) a.displayPercentage = minWidth;
-      return a;
-    });
-
-    return artworks;
-  }
-
-  const maxWidth = 1600;
-
-  const art = require('./manifest.json').art;
-
-  const updatedArtworks = calculateDisplaySizes(art);
-
-  res.render('pages/home', { art: updatedArtworks });
+  res.render('pages/home');
 });
 
 app.get('/untitled', (_, res) =>{
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,pug,css,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
