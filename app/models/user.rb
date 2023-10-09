fd28d43e65a0d80d087c5eef17766596c67cfd13
George Zalepa <george.zalepa@gmail.com>
2023-10-08 22:10:48 -0400

    Add users and homepage

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# A user
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :registerable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+end
